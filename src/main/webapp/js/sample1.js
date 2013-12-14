a39a53c8e27a4ec30955bdb468a239f9a0c47a3e
yixblog <davepotter@163.com>
2013-12-14 12:05:22 +0800

    base crud(update not finished)

--- src/main/webapp/js/sample1.js
+++ src/main/webapp/js/sample1.js
@@ -1,19 +1,35 @@
 /**
  * Created by yixian on 13-12-11.
  */
-
 angular.module('ie7support', []).config(function ($sceProvider) {
     // Completely disable SCE to support IE7.
     $sceProvider.enabled(false);
 });
 function PhoneListCtrl($scope, $http) {
-    $http.get("data/xfdata.json").success(function (data) {
+    $http.post("data/xfdata.json",{}).success(function (data) {
         console.log(data);
         $scope.xflist = data.xflist;
     });
 
-    $scope.addItem = function () {
+    $scope.totalAmount = function () {
+        var total = 0;
+        angular.forEach($scope.xflist, function (item) {
+            total += item.amount;
+        })
+        return total;
+    }
+    $scope.addItem = function (item) {
+        item.amount = parseInt(item.amount);
+        $scope.xflist.push(item);
+    }
 
+    $scope.deleteItem = function(index,item){
+        console.log(item);
+        $scope.xflist.splice(index,1);
+    }
+
+    $scope.sortBy = function(key){
+        $scope.orderColumn = key;
     }
 }
 
